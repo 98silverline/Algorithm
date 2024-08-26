d7b2ecc24a3829d9d284677d84ee2a017f32c85b
silverline <107904583+98silverline@users.noreply.github.com>
2024-08-26 15:04:01 +0900

    [Gold IV] Title: 단어 수학, Time: 100 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.java
+++ 백준/Gold/1339. 단어 수학/단어 수학.java
@@ -1,35 +1,74 @@
-
-import java.net.Inet4Address;
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    static int N, answer;
-    static PriorityQueue<Integer> pq;
-    static HashMap<Character, Integer> map;
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(bf.readLine());
-        map = new HashMap<>();
-        pq = new PriorityQueue<>(Collections.reverseOrder());
-        for (int i = 0; i < N; i++) {
-            String str = bf.readLine();
-            int n = 1;
-            for (int j = str.length() - 1; j >= 0; j--) {
-                map.put(str.charAt(j), map.getOrDefault(str.charAt(j), 0) + n);
-                n *= 10;
-            }
-        }
+	static int N;
+	static int[] importance = new int[26];
+	static int[] assign = new int[26];
+	static int[] numArr = new int[10];
+	static char[][] inputArr;
+	static boolean[] available = new boolean[10];
+	static int answer;
 
-        for (Character key : map.keySet()) {
-            pq.add(map.get(key));
-        }
-        int cnt = 9;
-        while(!pq.isEmpty()) {
-            answer += pq.poll() * cnt;
-            cnt--;
-        }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		inputArr = new char[N][];
+		answer = 0;
 
-        System.out.println(answer);
-    }
+		for(int i = 9; i >= 0; i--) {
+			numArr[9-i] = i;
+		}
+
+		for(int i = 0; i < N; i++) {
+			inputArr[i] = br.readLine().toCharArray();
+
+			for(int j = 0; j < inputArr[i].length; j++) {
+				importance[inputArr[i][inputArr[i].length - j - 1] - 'A'] += Math.pow(10, j);
+			}
+		}
+
+		getAnswer();
+		System.out.println(answer);
+	}
+
+	public static void getAnswer() {
+		
+		for(int j = 0; j < 26; j++) {
+			int maxNum = -1;
+			int numIndex = 0;
+
+			for(int i = 0; i < 26; i++) {
+				if(importance[i] > maxNum && importance[i] != 0) {
+					maxNum = importance[i];
+					numIndex = i;
+				}
+			}
+			
+			if(maxNum == -1) {
+				break;
+			}
+
+			for(int i = 0 ; i< 10; i++) {
+				if(!available[i]) {
+					assign[numIndex] = numArr[i];
+					importance[numIndex] = 0;
+					available[i] = true;
+					break;
+				}
+			}
+		}
+		
+		
+		for(int i = 0; i < N; i++) {
+			int temp = 0;
+			for(int j = 0; j < inputArr[i].length; j++) {
+				temp += assign[inputArr[i][inputArr[i].length - j - 1] - 'A'] * Math.pow(10, j);
+			}
+			answer += temp;
+		}
+
+	}
+
 }
