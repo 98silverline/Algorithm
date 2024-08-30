701f388da8b155ccafca6787c9a5be58b60a8ec5
silverline <107904583+98silverline@users.noreply.github.com>
2024-08-30 16:36:48 +0900

    [D4] Title: 최소 스패닝 트리, Time: 3,730 ms, Memory: 183,572 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -2,49 +2,43 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
+	/*
+	 * 메모리: 114,644 kb
+	 * 시간: 2,087 ms
+	 * 프림 알고리즘을 이용하였습니다.
+	 * */
 	static int T, V, E;
+	static int[] ch;
 	static long answer;
-	static int[] parent;
-	static ArrayList<Point> arr;
+	static ArrayList<ArrayList<Edge>> graph;
+	static PriorityQueue<Edge> pq;
 	
-	static class Point implements Comparable<Point>{
+	static class Edge implements Comparable<Edge>{
 		int x;
-		int y;
 		int cost;
-		public Point(int x, int y, int cost) {
+		public Edge(int x, int cost) {
 			this.x = x;
-			this.y = y;
 			this.cost = cost;
 		}
 		@Override
-		public int compareTo(Point o) {
+		public int compareTo(Edge o) {
 			return this.cost - o.cost;
 		}
 	}
-	
-	
-	static boolean Union(int x, int y) {
-		x = find(x);
-		y = find(y);
-		if(x == y) return false;
-		else if (x < y) parent[y] = x;
-		else parent[x] = y;
-		return true;
-	}
-	
-	static int find(int x) {
-		if(x == parent[x]) return x;
-		else return find(parent[x]);
-	}
-	
+
 	static void solution() {
-		int cnt = 1;
-		for(int i = 0; i < E; i++) {
-			if(cnt == V) return;
-			Point p = arr.get(i);
-			if(Union(p.x, p.y)) {
-				answer += p.cost;
+		int cnt = 0;
+		pq.offer(new Edge(1, 0));
+		while(!pq.isEmpty() && cnt < V) {
+			Edge tmp = pq.poll();
+			int ev = tmp.x;
+			if(ch[ev] == 0) {
+				ch[ev] = 1;
+				answer += tmp.cost;
 				cnt++;
+				for(Edge ob : graph.get(ev)) {
+					if(ch[ob.x] == 0) pq.offer(ob);
+				}
 			}
 		}
 	}
@@ -58,18 +52,19 @@ public class Solution {
 			StringTokenizer st = new StringTokenizer(bf.readLine());
 			V = Integer.parseInt(st.nextToken());
 			E = Integer.parseInt(st.nextToken());
-			parent = new int[V + 1];
-			arr = new ArrayList<>();
+			pq = new PriorityQueue<>();
+			graph = new ArrayList<>();
 			answer = 0;
-			for(int i = 0; i <= V; i++) parent[i] = i;
+			ch = new int[V + 1];
+			for(int i = 0; i <= V; i++) graph.add(new ArrayList<>());
 			for(int i = 0; i < E; i++) {
 				st = new StringTokenizer(bf.readLine());
-				int x = Integer.parseInt(st.nextToken());
-				int y = Integer.parseInt(st.nextToken());
-				int cost = Integer.parseInt(st.nextToken());
-				arr.add(new Point(x, y, cost));
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				int c = Integer.parseInt(st.nextToken());
+				graph.get(a).add(new Edge(b, c));
+				graph.get(b).add(new Edge(a, c));
 			}
-			Collections.sort(arr);
 			solution();
 			sb.append(answer + "\n");
 		}
