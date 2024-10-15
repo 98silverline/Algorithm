6bf6c1111232411b1972daae290aa253f502d43d
silverline <107904583+98silverline@users.noreply.github.com>
2024-10-15 11:40:28 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 128 ms, Memory: 16012 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int N, lisLength;
+	static int[] arr, lisIndex, lis;
+	static ArrayList<Integer> dp;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(bf.readLine());
+		lisLength = 0;
+		dp = new ArrayList<>();
+		arr = new int[N];
+		lisIndex = new int[N];
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			int n = arr[i];
+			int idx = Collections.binarySearch(dp, n);
+			if(idx < 0) idx = -(idx + 1);
+			if(idx == dp.size()) dp.add(n);
+			else dp.set(idx, n);
+			lisIndex[i] = idx;
+			lisLength = Math.max(lisLength, idx + 1);
+		}
+		
+		lis = new int[lisLength];
+		for(int i = N - 1; i >= 0; i--) {
+			if(lisIndex[i] == lisLength - 1) {
+				lis[lisLength - 1] = arr[i];
+				lisLength--;
+			}
+		}
+		System.out.println(dp.size());
+		for(int n : lis) System.out.print(n + " ");
+	}
+}
+
