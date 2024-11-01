a735d2f53631210e66937e8ffdf2d31c487359ff
silverline <107904583+98silverline@users.noreply.github.com>
2024-11-01 09:05:55 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 568 ms, Memory: 55948 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,72 +1,65 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int V, E;
-	static long answer;
-	static int[] parent;
-	static ArrayList<Point> arr;
-	
-	static class Point implements Comparable<Point>{
-		int x;
-		int y;
-		int cost;
-		public Point(int x, int y, int cost) {
-			this.x = x;
-			this.y = y;
-			this.cost = cost;
-		}
-		@Override
-		public int compareTo(Point o) {
-			return this.cost - o.cost;
-		}
-	}
-	
-	
-	static boolean Union(int x, int y) {
-		x = find(x);
-		y = find(y);
-		if(x == y) return false;
-		else if (x < y) parent[y] = x;
-		else parent[x] = y;
-		return true;
-	}
-	
-	static int find(int x) {
-		if(x == parent[x]) return x;
-		else return find(parent[x]);
-	}
-	
-	static void solution() {
-		int cnt = 1;
-		for(int i = 0; i < E; i++) {
-			if(cnt == V) return;
-			Point p = arr.get(i);
-			if(Union(p.x, p.y)) {
-				answer += p.cost;
-				cnt++;
-			}
-		}
-	}
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
-		parent = new int[V + 1];
-		arr = new ArrayList<>();
-		answer = 0;
-		for(int i = 0; i <= V; i++) parent[i] = i;
-		for(int i = 0; i < E; i++) {
-			st = new StringTokenizer(bf.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			int cost = Integer.parseInt(st.nextToken());
-			arr.add(new Point(x, y, cost));
-		}
-		Collections.sort(arr);
-		solution();
-		System.out.println(answer);
-	}
+    static int V, E, answer;
+    static boolean[] visited;
+    static PriorityQueue<Node> pq;
+    static ArrayList<ArrayList<Node>> graph;
+
+    static class Node implements Comparable<Node>{
+        int point;
+        int cost;
+
+        public Node(int point, int cost) {
+            this.point = point;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.cost - o.cost;
+        }
+    }
+
+    static void solution() {
+        pq = new PriorityQueue<>();
+        pq.offer(new Node(1, 0));
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
+            if(visited[node.point]) continue;
+            visited[node.point] = true;
+            answer += node.cost;
+            ArrayList<Node> curGraph = graph.get(node.point);
+            for (Node cur : curGraph) {
+                if (!visited[cur.point]) pq.offer(cur);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        V = Integer.parseInt(st.nextToken()); //정점의 개수
+        E = Integer.parseInt(st.nextToken()); //간선의 개수
+        graph = new ArrayList<>();
+        visited = new boolean[V + 1];
+        for (int i = 0; i <= V; i++) graph.add(new ArrayList<>());
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int node1 = Integer.parseInt(st.nextToken());
+            int node2 = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            graph.get(node1).add(new Node(node2, cost));
+            graph.get(node2).add(new Node(node1, cost));
+        }
+
+        solution();
+        System.out.println(answer);
+    }
 }
