f97e312be245684a8634076c5c330a07eff0cf28
SilverLine <107904583+98silverline@users.noreply.github.com>
2025-04-20 18:56:20 +0900

    [level 0] Title: 홀짝 구분하기, Time: 145.45 ms, Memory: 66.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(bf.readLine());
+        if(num % 2 == 0) System.out.println(num + " is even");
+        else System.out.println(num + " is odd");
+    }
+}
