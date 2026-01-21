879216f22c6beded47c6f8c73f5910e3b03831b6
SilverLine <107904583+98silverline@users.noreply.github.com>
2026-01-21 15:40:44 +0900

    Time: 2 ms (99.15%), Space: 45 MB (99.88%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.java
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.java
@@ -2,20 +2,14 @@ import java.util.*;
 import java.io.*;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        boolean flag = false;
+        HashMap<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++) {
-            for(int j = 0; j < nums.length; j++) {
-                if(i == j) continue;
-                if(nums[i] + nums[j] == target) {
-                    result[0] = i;
-                    result[1] = j;
-                    flag = true;
-                    break;
-                }
-                if(flag) break;
+            int cur = target - nums[i];
+            if(map.containsKey(cur)) {
+                return new int[] {map.get(cur), i};
             }
+            map.put(nums[i], i);
         }
-        return result;
+        return new int[] {};
     }
 }
