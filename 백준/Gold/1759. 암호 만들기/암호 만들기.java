f3a73fd0e996dd86c7a73c2a525ce239e765382b
SilverLine <107904583+98silverline@users.noreply.github.com>
2026-02-27 13:05:10 +0900

    [Gold V] Title: 암호 만들기, Time: 112 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -2,47 +2,49 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N, M;
-	static String str;
-	static char[] origin, arr;
-	static int[] ch;
-	static ArrayList<String> answer;
-	
-	public static void DFS(int L, char[] cArr, int num) {
-		if(L == N) {
-			Arrays.sort(cArr);
-			String ans = String.valueOf(cArr);
-			int cnt = 0;
-			for(int i = 0; i < N; i++ ) {
-				if(cArr[i] == 'a' || cArr[i] == 'e' || cArr[i] == 'i' || cArr[i] == 'o' || cArr[i] == 'u') cnt++;
-			}
-			if(cnt > 0 && (N - cnt) >= 2) answer.add(ans); 
-		}
-		else {
-			for(int i = num; i < M; i++) {
-				cArr[L] = str.charAt(i);
-				DFS(L + 1, cArr, i + 1);
-			}
-		}
-	}
-	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr = new char[N];
-		answer = new ArrayList<>();
-		str = "";
-		
-		st = new StringTokenizer(bf.readLine());
-		for(int i = 0; i < M; i++) {
-			str += st.nextToken();
-		}
-		
-		origin = str.toCharArray();
-		Arrays.sort(origin);
-		str = String.valueOf(origin);
-		DFS(0, arr, 0);
-		for(String string : answer) System.out.println(string);
-	}
+    static int L, C;
+    static char[] list;
+    static List<Character> vowel = new ArrayList<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+    static StringBuilder sb;
+
+    static void DFS(int cnt, int idx, int flag, char[] password) {
+        if(cnt == L) {
+            if(flag > 0 && L - flag >= 2) {
+                for(char c : password) {
+                    sb.append(c);
+                }
+                sb.append("\n");
+            }
+        } else {
+            for(int i = idx; i < C; i++) {
+                password[cnt] = list[i];
+                if (vowel.contains(list[i])) {
+                    DFS(cnt + 1, i + 1, flag + 1, password);
+                } else {
+                    DFS(cnt + 1, i + 1, flag, password);
+                }
+            }
+        }
+    }
+
+    static void solution() {
+        DFS(0, 0, 0, new char[L]);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        L = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        list = new char[C];
+        sb = new StringBuilder();
+
+        st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < C; i++) list[i] = st.nextToken().charAt(0);
+
+        Arrays.sort(list);
+
+        solution();
+        System.out.println(sb);
+    }
 }
